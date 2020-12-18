be27a432edfc66ebd5d792e1f8ee6245e3160913
Momen3030 <momenadel3030@gmail.com>
2020-12-18 15:13:40 +0200

    v3

--- src/components/branching-drawer/branching-drawer.tsx
+++ src/components/branching-drawer/branching-drawer.tsx
@@ -8,49 +8,95 @@ import { Component, h, Prop } from '@stencil/core';
 
 export class BranchingDrawer {
   @Prop() branchingNumber:number;
-  branchingArr:any[]=[
-    {
-      id:"tab-1",
-      contentLabel:"Test0",
-      highlightText:"terrwerwe0",
-      contant:"Test0 Contant",
-      dataTip:"fsdfsdf0",
-      qn:4,
-      questions:[
-    {
-      q1:
-      {id:"question-1",
-      contant:"Test1 Contant",
-      contentLabel:"Test1",
-      highlightText:"terrwerwe1",
-      dataTip:"fsdfsdf1"
-      }
-    }
-    ,
-    {
-        q2:
-        {id:"question-2",
-        contant:"Test2 Contant",
-        contentLabel:"Test2",
-        highlightText:"terrwerwe2",
-        dataTip:"fsdfsdf2"
-        }
-    }  
-    
-    
-    
-    
-    
-    ],
 
-    }
-  ];
+  branchingArr:any[]=[1,2,3,4,5];
 
 constructor(){
+    if(this.branchingArr.length!=this.branchingNumber){
+      if(this.branchingNumber==1){
+        this.branchingArr.length=0;
+        console.log(this.branchingNumber);
+        console.log(this.branchingArr);
+        
+        this.branchingArr.push(
+          {
+            id:"tab-1",
+            contentLabel:"Test01",
+            highlightText:"terrwerwe0",
+            contant1:"Test0 Contant",
+            dataTip:"fsdfsdf0",
+            questions:[
+          {q:
+            {id:"question-1",
+            contant:"Test1 Contant",
+            contentLabel:"Test1",
+            highlightText:"terrwerwe1",
+            dataTip:"fsdfsdf1"
+            }
 
+          }
+
+
+
+        ]
+          });
+      }
+    }
 }
 
   render() {
+    var  content2=[];
+   var qs=[];
+    this.branchingArr.map((item)=>{
+      // console.log(item.contant1);
+        if(item.questions.length==0){
+           content2.push(
+        <div class="content">
+          {item.contant1}
+          {item.highlightText!=null?
+           <span class="tip" data-tip={item.dataTip}> 
+              {item.highlightText}
+         </span>
+       :''}
+       
+      </div>
+            ) 
+        }
+        else{
+       
+       {item.questions.map(item2=>
+        // console.log(item2.q.id)
+        // console.log(item2.q.id);
+        qs.push(
+          <div class="questions">
+          <div class="question-wrap">
+            <input type="radio" id={item2.q.id} name="question"></input>
+            <label htmlFor={item2.q.id}><div>{item2.q.contentLabel}</div> <div class="cross"></div></label>
+            <div class="content">
+            {item2.q.highlightText!=null?
+           <span class="tip" data-tip={item.dataTip}> 
+              {item2.q.highlightText}
+         </span>
+       :''}
+                {item2.q.contant}
+              
+              </div>
+             
+          </div>
+          </div>
+        
+
+        )
+        
+        
+        )}
+
+
+
+        } 
+   
+
+    });
     return (
       <div class="wrapper">
        {/* <div class="wrap-1">
@@ -91,10 +137,44 @@ constructor(){
         </div>
       </div>
     </div> */}
+   
+
+   {this.branchingArr.map((item)=>
+      <div class="wrap-3">
+      <input type="radio" id={item.id} name="tabs"></input>
+      <label htmlFor={item.id}><div> {item.contentLabel}</div><div class="cross"></div></label>
+            {content2[0]!=null?content2[0]:''}
+            {qs?(qs.map(i=>i)):''}
+
+      {/* <div class="questions">
+        <div class="question-wrap">
+          <input type="radio" id="question-9" name="question"></input>
+          <label htmlFor="question-9"><div> تشعب آخر</div> <div class="cross"></div></label>
+          <div class="content">
+          <span class="tip" data-tip={ <text>توضيح لمعنى الكلمة الغريبة</text>}> 
+          كلمة غريبة تحتاج إلى تفسير
+        </span>
+            
+            هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق.</div>
+           
+        </div>
+        <div class="question-wrap">
+          <input type="radio" id="question-" name="question"></input>
+          <label htmlFor="question-4"><div>تشعب آخر </div> <div class="cross"></div></label>
+          <div class="content">هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق.</div>
+  
+        </div>
+      </div> */}
+    </div>
 
 
+   
+   
+   
+   )}
 
-    <div class="wrap-3">
+
+    {/* <div class="wrap-3">
       <input type="radio" id="tab-4" name="tabs"></input>
       <label htmlFor="tab-4"><div>العنوان الرابع</div><div class="cross"></div></label>
       <div class="questions">
@@ -116,34 +196,7 @@ constructor(){
   
         </div>
       </div>
-    </div>
-
-
-
-
-    <div class="wrap-3">
-      <input type="radio" id="tab-8" name="tabs"></input>
-      <label htmlFor="tab-8"><div>العنوان الرابع</div><div class="cross"></div></label>
-      <div class="questions">
-        <div class="question-wrap">
-          <input type="radio" id="question-9" name="question"></input>
-          <label htmlFor="question-9"><div> تشعب آخر</div> <div class="cross"></div></label>
-          <div class="content">
-          <span class="tip" data-tip={ <text>توضيح لمعنى الكلمة الغريبة</text>}> 
-          كلمة غريبة تحتاج إلى تفسير
-        </span>
-            
-            هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق.</div>
-           
-        </div>
-        <div class="question-wrap">
-          <input type="radio" id="question-" name="question"></input>
-          <label htmlFor="question-4"><div>تشعب آخر </div> <div class="cross"></div></label>
-          <div class="content">هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق.</div>
-  
-        </div>
-      </div>
-    </div>
+    </div> */}
 
 
 

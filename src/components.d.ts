fcc9b492d38e624f0688c10181af5047b2d3f969
Momen3030 <momenadel3030@gmail.com>
2020-12-18 11:52:47 +0200

    v2

--- src/components.d.ts
+++ src/components.d.ts
@@ -6,6 +6,9 @@
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
+    interface BranchingDrawer {
+        "branchingNumber": number;
+    }
     interface MyComponent {
         /**
           * The first name
@@ -22,6 +25,12 @@ export namespace Components {
     }
 }
 declare global {
+    interface HTMLBranchingDrawerElement extends Components.BranchingDrawer, HTMLStencilElement {
+    }
+    var HTMLBranchingDrawerElement: {
+        prototype: HTMLBranchingDrawerElement;
+        new (): HTMLBranchingDrawerElement;
+    };
     interface HTMLMyComponentElement extends Components.MyComponent, HTMLStencilElement {
     }
     var HTMLMyComponentElement: {
@@ -29,10 +38,14 @@ declare global {
         new (): HTMLMyComponentElement;
     };
     interface HTMLElementTagNameMap {
+        "branching-drawer": HTMLBranchingDrawerElement;
         "my-component": HTMLMyComponentElement;
     }
 }
 declare namespace LocalJSX {
+    interface BranchingDrawer {
+        "branchingNumber"?: number;
+    }
     interface MyComponent {
         /**
           * The first name
@@ -48,6 +61,7 @@ declare namespace LocalJSX {
         "middle"?: string;
     }
     interface IntrinsicElements {
+        "branching-drawer": BranchingDrawer;
         "my-component": MyComponent;
     }
 }
@@ -55,6 +69,7 @@ export { LocalJSX as JSX };
 declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
+            "branching-drawer": LocalJSX.BranchingDrawer & JSXBase.HTMLAttributes<HTMLBranchingDrawerElement>;
             "my-component": LocalJSX.MyComponent & JSXBase.HTMLAttributes<HTMLMyComponentElement>;
         }
     }
--- src/components/branching-drawer/branching-drawer.tsx
+++ src/components/branching-drawer/branching-drawer.tsx
@@ -1,17 +1,160 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, h, Prop } from '@stencil/core';
 
 @Component({
   tag: 'branching-drawer',
   styleUrl: 'branching-drawer.css',
   shadow: true,
 })
+
 export class BranchingDrawer {
+  @Prop() branchingNumber:number;
+  branchingArr:any[]=[
+    {
+      id:"tab-1",
+      contentLabel:"Test0",
+      highlightText:"terrwerwe0",
+      contant:"Test0 Contant",
+      dataTip:"fsdfsdf0",
+      qn:4,
+      questions:[
+    {
+      q1:
+      {id:"question-1",
+      contant:"Test1 Contant",
+      contentLabel:"Test1",
+      highlightText:"terrwerwe1",
+      dataTip:"fsdfsdf1"
+      }
+    }
+    ,
+    {
+        q2:
+        {id:"question-2",
+        contant:"Test2 Contant",
+        contentLabel:"Test2",
+        highlightText:"terrwerwe2",
+        dataTip:"fsdfsdf2"
+        }
+    }  
+    
+    
+    
+    
+    
+    ],
+
+    }
+  ];
+
+constructor(){
+
+}
 
   render() {
     return (
-      <Host>
-        <slot></slot>
-      </Host>
+      <div class="wrapper">
+       {/* <div class="wrap-1">
+      <input type="radio" id="tab-1" name="tabs"></input>
+      <label htmlFor="tab-1"><div>العنوان الأول</div><div class="cross"></div></label>
+      <div class="content">هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق.</div>
+    </div> */}
+{/*   
+    <div class="wrap-2">
+      <input type="radio" id="tab-2" name="tabs"></input>
+      <label htmlFor="tab-2"><div>العنوان الثاني</div><div class="cross"></div></label>
+      <div class="content">
+        هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق.
+        <span class="tip" data-tip=" توضيح لمعنى الكلمة الغريبة"> 
+          كلمة غريبة تحتاج إلى تفسير
+        </span>
+      </div>
+       
+    </div> */}
+  
+
+
+    {/* <div class="wrap-3">
+      <input type="radio" id="tab-3" name="tabs"></input>
+      <label htmlFor="tab-3"><div>العنوان الثالث</div><div class="cross"></div></label>
+      <div class="questions">
+        <div class="question-wrap">
+          <input type="radio" id="question-1" name="question"></input>
+          <label htmlFor="question-1"><div>عنوان أول متشعب من الثالث</div> <div class="cross"></div></label>
+          <div class="content">هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق.</div>
+  
+        </div>
+        <div class="question-wrap">
+          <input type="radio" id="question-2" name="question"></input>
+          <label htmlFor="question-2"><div>عنوان ثاني متشعب من الثالث</div> <div class="cross"></div></label>
+          <div class="content">هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق.</div>
+  
+        </div>
+      </div>
+    </div> */}
+
+
+
+    <div class="wrap-3">
+      <input type="radio" id="tab-4" name="tabs"></input>
+      <label htmlFor="tab-4"><div>العنوان الرابع</div><div class="cross"></div></label>
+      <div class="questions">
+        <div class="question-wrap">
+          <input type="radio" id="question-3" name="question"></input>
+          <label htmlFor="question-3"><div> تشعب آخر</div> <div class="cross"></div></label>
+          <div class="content">
+          <span class="tip" data-tip={ <text>توضيح لمعنى الكلمة الغريبة</text>}> 
+          كلمة غريبة تحتاج إلى تفسير
+        </span>
+            
+            هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق.</div>
+           
+        </div>
+        <div class="question-wrap">
+          <input type="radio" id="question-4" name="question"></input>
+          <label htmlFor="question-4"><div>تشعب آخر </div> <div class="cross"></div></label>
+          <div class="content">هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق.</div>
+  
+        </div>
+      </div>
+    </div>
+
+
+
+
+    <div class="wrap-3">
+      <input type="radio" id="tab-8" name="tabs"></input>
+      <label htmlFor="tab-8"><div>العنوان الرابع</div><div class="cross"></div></label>
+      <div class="questions">
+        <div class="question-wrap">
+          <input type="radio" id="question-9" name="question"></input>
+          <label htmlFor="question-9"><div> تشعب آخر</div> <div class="cross"></div></label>
+          <div class="content">
+          <span class="tip" data-tip={ <text>توضيح لمعنى الكلمة الغريبة</text>}> 
+          كلمة غريبة تحتاج إلى تفسير
+        </span>
+            
+            هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق.</div>
+           
+        </div>
+        <div class="question-wrap">
+          <input type="radio" id="question-" name="question"></input>
+          <label htmlFor="question-4"><div>تشعب آخر </div> <div class="cross"></div></label>
+          <div class="content">هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق.</div>
+  
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+        </div>
     );
   }
 
